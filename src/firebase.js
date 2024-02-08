e309fe6bb7844e33039f2d4a72d4eb11abd7f505
Enes YILDIRIM <enesfly@192.168.1.87>
2024-02-09 02:26:21 +0300

    firebase: added hooks and initialized firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCHmmhDWtLLUasuddqk3bsVcsvYCp66woE",
+  authDomain: "match-medic-p0.firebaseapp.com",
+  projectId: "match-medic-p0",
+  storageBucket: "match-medic-p0.appspot.com",
+  messagingSenderId: "793569075086",
+  appId: "1:793569075086:web:86059d65574dbaa3214edf",
+  measurementId: "G-4Z48RHHR20"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/layout/LoginForm.jsx
+++ src/layout/LoginForm.jsx
@@ -1,88 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Box from '@mui/system/Box';
 import TextField from '@mui/material/TextField';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import Button from '@mui/material/Button'
+import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
 const LoginForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-const inputProps = {
+  const inputProps = {
     textInput: {
-        style:{
-            borderRadius:"3em",
+      style: {
+        borderRadius: "3em",
+      }
+    }
+  };
 
-        }} };
-const styles={
-    textInput:{width:"20rem",
-    
-  }
-}
-  return <Box sx={{
-    padding:"2rem",
-    borderTop:"1px solid rgb(204, 204, 204)",
-    borderBottom:"1px solid rgb(204, 204, 204)",
+  const styles = {
+    textInput: { width: "20rem" }
+  };
+
+  return (
+    <Box sx={{
+      padding: "2rem",
+      borderTop: "1px solid rgb(204, 204, 204)",
+      borderBottom: "1px solid rgb(204, 204, 204)",
     }}
-    justifyContent={"center"}
-    alignContent={"center"}
-    display={"flex"}
-    flexDirection={"column"}
-    gap={5}>
-    <Typography variant='h5' align='center'> Get Started</Typography>
-    <Stack
-      justifyContent="center"
-      direction="row"
-      gap={5}
-    >
-      <Stack sx={{
-        border: "1px solid rgb(204, 204, 204)",
-        padding:"2rem",
-        borderRadius:"3em",
-        boxShadow:" 0 2px 4px rgba(0, 0, 0, .2)",
+      justifyContent={"center"}
+      alignContent={"center"}
+      display={"flex"}
+      flexDirection={"column"}
+      gap={5}>
+      <Typography variant='h5' align='center'>Get Started</Typography>
+      <Stack
+        justifyContent="center"
+        direction="row"
+        gap={5}
+      >
+        <Stack sx={{
+          border: "1px solid rgb(204, 204, 204)",
+          padding: "2rem",
+          borderRadius: "3em",
+          boxShadow: " 0 2px 4px rgba(0, 0, 0, .2)",
         }}
-        direction="column"
-        justifyContent="space-between"
-        alignItems="center"
-        gap={1}
-      
-
+          direction="column"
+          justifyContent="space-between"
+          alignItems="center"
+          gap={1}
         >
-          <Typography 
+          <Typography
             variant='h6'
-            align='center'             
-            >
-              Create an account
-            </Typography>
-          
-            
-            <TextField 
+            align='center'
+          >
+            Create an account
+          </Typography>
+
+          <TextField
             sx={styles.textInput}
             InputProps={inputProps.textInput}
-            id="outlined-basic" 
-            label="name" 
+            label="Name"
             variant="outlined"
             size='small'
-            />
-            
-            <TextField 
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+
+          <TextField
             sx={styles.textInput}
             InputProps={inputProps.textInput}
-            id="outlined-basic" 
-            label="email address" 
+            label="Email Address"
             variant="outlined"
             size='small'
-            />
-            <TextField 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <TextField
             sx={styles.textInput}
             InputProps={inputProps.textInput}
-            id="outlined-basic" 
-            label="password"
+            label="Password"
             variant="outlined"
             size='small'
-            />
-            
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
           
           <FormControlLabel  control={<Checkbox defaultChecked />} flexWrap={"wrap"} label="lorem ipsum da ipsum da ipsum" />
           <Button variant="contained" color="inherit" sx={{borderRadius:"3em"}}>Login</Button>
@@ -134,9 +140,10 @@ const styles={
           
           
           <Button color="inherit" variant="contained" sx={{borderRadius:"3em"}}> Log in</Button>
+        </Stack>
       </Stack>
-    </Stack>
-  </Box>
+    </Box>
+  );
 }
 
-export default LoginForm
+export default LoginForm;
